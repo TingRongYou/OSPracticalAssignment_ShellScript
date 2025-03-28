6d903521f6b37cc17143cbee11c05f5f9f714acf
TingRongYou <ryting999@gmail.com>
2025-03-28 19:14:46 +0000

    Add New Patron Details V1

--- shell.sh
+++ shell.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-display_menu() {
+main_menu() {
+    clear
     echo "Patron Maintenance Menu"
+    echo
     echo "A - Add New Patron Details"
     echo "S - Search A Patron (by Patron ID)"
     echo "U - Update a Patron Details"
@@ -11,8 +13,106 @@ display_menu() {
     echo "J - Sort Patrons by Joined Date (Newest to Oldest Date)"
     echo
     echo "Q - Exit from Program"
-    echo
-    read -p 'Please Select A Choice: ' menu_choice
+
+    isValidChoice=true
+
+    while $isValidChoice; do
+        echo
+        read -p 'Please Select A Choice: ' menu_choice
+        case ${menu_choice^^} in 
+        "A")
+        isValidChoice=false
+        add_patron_form
+        ;;
+        "S")
+        isValidChoice=false
+        #Seach
+        ;;
+        "U")
+        isValidChoice=false
+        #Update
+        ;;
+        "D")
+        isValidChoice=false
+        #Delete
+        ;;
+        "L")
+        isValidChoice=false
+        #Sort(LastName)
+        ;;
+        "P")
+        isValidChoice=false
+        #Sort(PatronID)
+        ;;
+        "J")
+        isValidChoice=false
+        #Sort(Date)
+        ;;
+        "Q")
+        isValidChoice=false
+        exit 0
+        ;;
+        *)
+        echo ">>> Please enter valid choice (A, S, U, D, L, P, J, Q)!"
+        ;;
+        esac
+    done
 }
 
-display_menu
+add_patron_form() {
+    clear
+    echo "Add New Patron Form"
+    echo "===================="
+    echo "Patron ID: "
+    echo "First Name: "
+    echo "Last Name: "
+    echo "Mobile Number: "
+    echo "Birth Date (MM-DD-YYYY): "
+    echo "Membership type (Student / Public): "
+    echo "Joined Date (MM-DD-YYYY): "
+    echo
+    echo "Press (q) to return to Patron Maintenance Menu."
+
+    isValidChoice=true
+
+    while $isValidChoice; do 
+        echo
+        read -p 'Add another new patron details? (y)es or (q)uit: ' add_patron_choice
+        case ${add_patron_choice^^} in 
+        "Y")
+        isValidChoice=false
+        #AddNew 
+        ;;
+        "Q")
+        isValidChoice=false
+        exit 0
+        ;;
+        *)
+        echo ">>> Please enter valid choice (y, q)!"
+        ;;
+        esac
+    done
+}
+
+add_new_patron() {
+    clear
+    echo "Add New Patron Form"
+    echo "===================="
+
+    isValidPatronID=true
+
+    while $isValidPatronID; do
+        read -p 'Patron ID(eg: Pxxxx): ' patron_id
+    done
+    echo "First Name: "
+    echo "Last Name: "
+    echo "Mobile Number: "
+    echo "Birth Date (MM-DD-YYYY): "
+    echo "Membership type (Student / Public): "
+    echo "Joined Date (MM-DD-YYYY): "
+    echo
+    echo "Press (q) to return to Patron Maintenance Menu."
+}
+
+main_menu
+
